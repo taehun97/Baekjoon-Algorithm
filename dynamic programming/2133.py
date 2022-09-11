4de4f1b184e461ad5a74d0f4a66c4b8ade8164e7
taehun97 <zaqxsw1397@gmail.com>
2022-09-12 00:03:30 +0900

    feat: 2022/09/11 commit 2

--- dynamic programming/2133.py
+++ dynamic programming/2133.py
@@ -5,8 +5,10 @@ input = sys.stdin.readline
 n = int(input())
 dp = [1] + [0 for _ in range(n)]
 
+# dp[i] = 3 * dp[i-2] + 2 * (dp[i-4] + dp[i-6] + ... + dp[2] + dp[0])
 for i in range(2, n+1, 2):
-    if i==2: dp[i] = dp[i-2]*3
-    else: dp[i] = dp[i-4]*2 + dp[i-2]*3
+    dp[i] += 3*dp[i-2]
+    for k in range(i-4, -1, -2):
+        dp[i] += 2*dp[k]
     
 print(dp[n])
