351d4c7f59c2817dc11d47355b01ae38786b2307
taehun97 <zaqxsw1397@gmail.com>
2022-09-19 23:48:49 +0900

    fix: 2022/09/19 17425.py fixed

--- subtotal/17425.py
+++ subtotal/17425.py
@@ -2,24 +2,22 @@ import sys
 
 input = sys.stdin.readline
 
-def getDivisors(x):
-    divisor_list = []
-    for i in range(1, int(x**(1/2)) + 1):
-        if x%i==0:
-            divisor_list.append(i)
-            if x!=i**2: divisor_list.append(x//i)
-            
-    return divisor_list
+dp = [1 for _ in range(1000001)]
+for i in range(2, 1000001):
+    j = 1
+    while i*j<=1000000:
+        dp[i*j] += i
+        j += 1
+
+acumulative_sum = [0 for _ in range(1000001)]
+
+for i in range(1, 1000001):
+    acumulative_sum[i] = acumulative_sum[i-1] + dp[i]
 
 T = int(input())
-divisor_cumulative_sum = [0 for _ in range(1000001)]
+
 for _ in range(T):
     N = int(input())
     total_sum = 0
-
-    for i in range(1, N+1):
-        if divisor_cumulative_sum[i]==0: 
-            local_sum = sum(getDivisors(i))
-            divisor_cumulative_sum[i] = divisor_cumulative_sum[i-1] + local_sum
         
-    print(divisor_cumulative_sum[N])
+    print(acumulative_sum[N])
